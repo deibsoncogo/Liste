1d3863aca22810b0eda99455d9a20b41a1c109ae
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-09 16:18:23 -0300

    👽️ api: configured startup firebase

---
+++ src/service/index.ts
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app'
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyCWzjVedKDJA_cZ7pa1QRP7BiXB3ysmqAQ',
+  authDomain: 'rctliste.firebaseapp.com',
+  projectId: 'rctliste',
+  storageBucket: 'rctliste.appspot.com',
+  messagingSenderId: '139722638711',
+  appId: '1:139722638711:web:6b19f51d0c33822ce94d72',
+  measurementId: 'G-N2ECYPY4FY',
+}
+
+initializeApp(firebaseConfig)
