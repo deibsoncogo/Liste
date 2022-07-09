20ef2c335f4378cac9e0c7fddeb5f87ca799a42c
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-09 16:19:35 -0300

    🔒️ security: created authentication context

--- src/app.tsx
+++ src/app.tsx
@@ -1,13 +1,16 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import { AuthContextProvider } from './contexts/authContext'
 import { Home } from './pages/home/home'
 import './app.scss'
 
 export function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} caseSensitive />
-      </Routes>
+      <AuthContextProvider>
+        <Routes>
+          <Route path='/' element={<Home />} caseSensitive />
+        </Routes>
+      </AuthContextProvider>
     </BrowserRouter>
   )
 }
