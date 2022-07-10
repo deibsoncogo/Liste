b0041e8f405525fbf3dff161828534c88fc89598
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-10 17:52:44 -0300

    🚚 fix: implemention folder src

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import styles from '../styles/home.module.css'
+import styles from './home.module.css'
 
 export default function Home() {
   return (
