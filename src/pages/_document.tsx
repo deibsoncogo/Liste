8882e8010c52dd0caac165d70ca7dfc728d491e8
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-10 23:52:50 -0300

    🎉 feat: implemention index html

---
+++ src/pages/_document.tsx
@@ -0,0 +1,23 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+export default class MyDocument extends Document {
+  render() {
+    return (
+      <Html>
+        <Head>
+          <link rel='preconnect' href='https://fonts.googleapis.com' />
+          <link href='https://fonts.googleapis.com/css2?family=Arima:wght@100;400;700&family=Playball&family=Rubik+Mono+One&family=Russo+One&display=swap' rel='stylesheet' />
+
+          <link rel='shortcut icon' href='/favicon.ico' type='image/ico' />
+
+          <title>Liste</title>
+        </Head>
+
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    )
+  }
+}
