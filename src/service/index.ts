b3839c5912bf654032312f2ad5c7f5ad7c190d45
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-11 09:07:33 -0300

    🗃️ database: implemention database modo test

--- src/service/index.ts
+++ src/service/index.ts
@@ -8,6 +8,7 @@ const firebaseConfig = {
   messagingSenderId: '139722638711',
   appId: '1:139722638711:web:6b19f51d0c33822ce94d72',
   measurementId: 'G-N2ECYPY4FY',
+  databaseURL: 'https://rctliste-default-rtdb.firebaseio.com',
 }
 
 initializeApp(firebaseConfig)
