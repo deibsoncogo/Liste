99f3c328b0986a7077c3661f069b2889a1a1b894
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-10 21:08:27 -0300

    👽️ api: implemention firebase

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,11 @@
 import { AppProps } from 'next/app'
+import '../service/index'
 import './_app.scss'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <Component {...pageProps} />
+  )
 }
 
 export default MyApp
