8865c8f46f1302b5a5759159176d5a27dc641b70
Deibson Cogo <deibsoncogo@outlook.com>
2022-07-11 08:09:33 -0300

    👽️ api: corretion domain the image

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  images: { domains: ['storage.googleapis.com'] },
+  images: { domains: ['lh3.googleusercontent.com'] },
 }
 
 module.exports = nextConfig
