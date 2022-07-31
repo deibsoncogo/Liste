21d8397d9fa54dc5785b728a67b6f1c12770254d
Deibson Cogo <deibsoncogorotaract@outlook.com>
2022-07-31 20:11:03 -0300

    ✨ feat: creation save e get registration user

--- src/components/registration/index.tsx
+++ src/components/registration/index.tsx
@@ -1,30 +1,30 @@
-import { useState, FormEvent, useEffect } from 'react'
-import { child, get, getDatabase, ref, update } from 'firebase/database'
+import { useEffect, useState } from 'react'
 import { UseAuth } from '../../contexts/authContext'
+import { UseDatabase } from '../../contexts/databaseContext'
 import { Button } from '../button'
 import style from './style.module.scss'
 
+type TUserRegistration = {
+  organization: string
+  occupation: string
+}
+
 export function Registration() {
-  const { user } = UseAuth()
+  const { Upsert, Get } = UseDatabase()
+  const { userAuth } = UseAuth()
+
   const [organization, setOrganization] = useState('')
   const [occupation, setOccupation] = useState('')
 
-  async function UpsertRegistration(event: FormEvent): Promise<void> {
-    event.preventDefault()
-    await update(ref(getDatabase(), `users/${user.id}`), { organization, occupation })
-      .catch((error) => console.error('UpsertRegistration error =>', error))
-  }
-
   useEffect(() => {
-    get(child(ref(getDatabase()), `users/${user?.id}`))
-      .then((response) => {
-        if (response.exists()) {
-          setOrganization(response.val().organization)
-          setOccupation(response.val().occupation)
+    Get({ table: 'users', id: userAuth?.id })
+      .then((response: TUserRegistration) => {
+        if (response) {
+          setOrganization(response.organization)
+          setOccupation(response.occupation)
         }
       })
-      .catch((error) => console.error('Registration useEffect =>', error))
-  }, [user])
+  }, [Get, userAuth])
 
   return (
     <div className={style.registration}>
@@ -56,7 +56,10 @@ export function Registration() {
           type='submit'
           text='salvar'
           color='var(--pink1)'
-          onClick={(event: FormEvent) => UpsertRegistration(event)}
+          onClick={(event) => {
+            event.preventDefault()
+            Upsert({ table: 'users', id: userAuth.id, data: { organization, occupation } })
+          }}
         />
       </form>
     </div>
